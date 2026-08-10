19ac3b3515d4f1ba299126fb052d1b0d7620a66a
Luis Zadi <beawarumbyof@gmail.com>
2026-08-10 17:06:08 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,5 @@
-// Configuração do Supabase
-
-// URL do projeto Supabase
 export const SUPABASE_URL = "https://nrfbuwhcvwkdztnwlwzl.supabase.co";
 
-// Chave pública (anon/publishable)
-// Nunca coloque a service_role key aqui.
 export const SUPABASE_ANON_KEY = "sb_publishable_G1Hbog8gpp8Y0Jmk7Rg9hw_brDqs1HR";
 
-// E-mail do administrador
 export const ADMIN_EMAIL = "beawarumbyof@gmail.com";
