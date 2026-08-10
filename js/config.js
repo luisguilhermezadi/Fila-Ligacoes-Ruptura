caa90aef2d0b9fd7455b1910e32aa7f9a35bdb16
Luis Zadi <beawarumbyof@gmail.com>
2026-08-10 14:07:36 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Preencha com as informações do seu projeto Supabase.
 // NUNCA coloque a service_role key neste arquivo. Somente a chave anon/publishable.
-export const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SUPABASE";
-export const SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_ANON_PUBLIC";
+export const SUPABASE_URL = "https://nrfbuwhcvwkdztnwlwzl.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_G1Hbog8gpp8Y0Jmk7Rg9hw_brDqs1HR";
 export const ADMIN_EMAIL = "beawarumbyof@gmail.com";
